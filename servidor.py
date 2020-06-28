ca0858a499ea965b953f4df4398a69efc3dffa06
Sergio Leon <usac201700722>
2020-06-28 01:40:06 +0000

    Cambios realizados en el servidor, pero no se hara push desde aqui

--- servidor.py
+++ servidor.py
@@ -11,7 +11,40 @@ from comandos import *
 LOG_FILENAME = 'mqtt.log'
 SALAS_FILENAME = 'salas'
 USER_FILENAME = 'usuarios'
-dato=b''
+#dato=b''
+
+class MQTTconfig(mqtt.Client):
+    def on_connect(self, client, userdata, flags, rc):
+        #SALU Handler en caso suceda la conexion con el broker MQTT
+        connectionText = "CONNACK recibido del broker con codigo: " + str(rc)
+        logging.debug(connectionText)
+    def on_publish(self, client, userdata, mid): 
+        #SALU Handler en caso se publique satisfactoriamente en el broker MQTT
+        publishText = "Publicacion satisfactoria"
+        logging.debug(publishText)
+    def on_message(self, client, userdata, msg):	
+        #SALU Callback que se ejecuta cuando llega un mensaje al topic suscrito
+        #SALU msg contiene el topic y la info que llego
+        #SALU Se muestra en pantalla informacion que ha llegado
+        #Se muestra en pantalla informacion que ha llegado
+        #global dato 
+        dato = msg.payload
+        logging.debug("Ha llegado el mensaje al topic: " + str(msg.topic)) #de donde vino el mss
+        logging.debug("El contenido del mensaje es: " + str(dato.decode('utf-8')))#que vino en el mss
+        comandosEntrada(dato)
+            
+    def on_subscribe(self, client, obj,mid, qos):
+        #SALU Handler en caso se suscriba satisfactoriamente en el broker MQTT
+        logging.debug("Suscripcion satisfactoria")
+
+    def run(self):
+        #SALU este metodo inicializa la conexion MQTT con las credenciales del broker
+        self.username_pw_set(MQTT_USER, MQTT_PASS)
+        self.connect(host=MQTT_HOST, port = MQTT_PORT)        
+        rc = 0
+        while rc==0:
+            rc = self.loop_start()
+        return rc
 
 class configuracionesServidor(object):
 
@@ -19,7 +52,7 @@ class configuracionesServidor(object):
         self.filename = filename
         self.qos = qos
 
-
+    
     def subSalas(self):
         datos = []
         archivo = open(self.filename,'r') 
@@ -29,9 +62,9 @@ class configuracionesServidor(object):
             datos.append(registro) 
         archivo.close() #Cerrar el archivo al finalizar
         for i in datos:
-            #client.subscribe(("salas/"+str(i[0])+"/S"+str(i[1]), qos))
-            logging.debug("salas/"+str(i[0])+"/S"+str(i[1]))
-    
+            client.subscribe(("comandos/"+str(i[0])+"/S"+str(i[1]), qos))
+            logging.debug("comandos/"+str(i[0])+"/S"+str(i[1]))
+    '''
     def subUsuarios(self):
         datos = []
         archivo = open(self.filename,'r') #Abrir el archivo en modo de LECTURA
@@ -43,7 +76,7 @@ class configuracionesServidor(object):
         for i in datos:
             client.subscribe(("usuarios/08/"+str(i[0]), self.qos))
             logging.debug("usuarios/08/"+str(i[0]))
-    
+    '''
     def subComandos(self):
         datos = []
         archivo = open(self.filename,'r') #Abrir el archivo en modo de LECTURA
@@ -64,38 +97,37 @@ class configuracionesServidor(object):
         return self.__str__
 
 class hiloTCP(object):
-    def __init__(self,IP_ADDR):
-        self.IP_ADDR=IP_ADDR
+    def __init__(self,topic):
+        self.topic=topic
         self.hiloRecibidor=threading.Thread(name = 'Guardar nota de voz',
                         target = hiloTCP.conexionTCP,
-                        args = (self,self.IP_ADDR),
-                        daemon = True
+                        args = (self,self.topic),
+                        daemon = False
                         )
         self.hiloEnviador=threading.Thread(name = 'Enviar nota de voz',
                         target = hiloTCP.conexionTCPenvio,
-                        args = (self,self.IP_ADDR),
-                        daemon = True
+                        args = (self,self.topic),
+                        daemon = False
                         )
 
-    def conexionTCP(self, IP_ADDR):
+    def conexionTCP(self, topic):
         # Crea un socket TCP
+        logging.info(self.topic)
+        okey = comandosCliente(self.topic)
+        print(okey.OK())
+        client.publish("comandos/08/201700722",okey.OK(),2,False)
+        
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-        self.IP_ADDR ='167.71.243.238' #La IP donde desea levantarse el server
-        #IP_ADDR_ALL = '' #En caso que se quiera escuchar en todas las interfaces de red
-        #IP_PORT = 9808 #Puerto al que deben conectarse los clientes
-        #BUFFER_SIZE = 64*1024
-
         # Bind the socket to the port
         serverAddress = (IP_ADDR_ALL, IP_PORT) #Escucha en todas las interfaces
         print('Iniciando servidor en {}, puerto {}'.format(*serverAddress))
         sock.bind(serverAddress) #Levanta servidor con parametros especificados
-
         # Habilita la escucha del servidor en las interfaces configuradas
         sock.listen(10) #El argumento indica la cantidad de conexiones en cola
         
         bandera = True
         while bandera==True:
+            
             # Esperando conexion
             logging.info('Esperando conexion remota')
             connection, clientAddress = sock.accept()
@@ -112,8 +144,8 @@ class hiloTCP(object):
                     logging.info('Transmision finalizada')
                     sock.close()
                     connection.close()
-                    time.sleep(10)
-                    enviar_nota_de_voz = hiloTCP(IP_ADDR)
+                    time.sleep(5)
+                    enviar_nota_de_voz = hiloTCP(self.topic)
                     enviar_nota_de_voz.hiloEnviador.start()
                     bandera = False
                     break
@@ -125,70 +157,68 @@ class hiloTCP(object):
                 # Se baja el servidor para dejar libre el puerto para otras aplicaciones o instancias de la aplicacion
                 connection.close()
 
-    def conexionTCPenvio(self, IP_ADDR):
+    def conexionTCPenvio(self, topic):
+        print(self.topic)
         SERVER_ADDR = '167.71.243.238'
         SERVER_PORT = 9808
         BUFFER_SIZE = 64 * 1024 
+        objeto= comandosCliente(self.topic)
+        fsize = os.stat('recibido.wav').st_size
         
-        nose=b'\x02$201700722'
-        client.publish("comandos/08/201700722",nose,2,False)
+        client.publish("comandos/08/201700722",objeto.fileReceive(fsize),2,False)
         server_socket = socket.socket()
         server_socket.bind((SERVER_ADDR, SERVER_PORT))
-        server_socket.listen(100) #1 conexion activa y 9 en cola
+        server_socket.listen(100)#1 conexion activa y 9 en cola
         try:
             while True:
-                print("\nEsperando conexion remota para enviar el archivo de audio: \n")
+                logging.info("\nEsperando conexion remota...\n")
                 conn, addr = server_socket.accept()
                 print('Conexion establecida desde ', addr)
-                print('Enviando archivo audio...')
+                logging.debug('Enviando archivo de audio...')
                 with open('recibido.wav', 'rb') as f: #Se abre el archivo a enviar en BINARIO
                     conn.sendfile(f, 0)
                     f.close()
                 conn.close()
                 print("\n\nArchivo enviado a: ", addr)
                 server_socket.close()
+                print("Cerrando el servidor...")
         finally:
             print("Cerrando el servidor...")
             server_socket.close()
 
+def comandosEntrada(dato):
+    comando_accion = comandosServidor(str(dato))
+    topic = comando_accion.separa()[1]
+    
+    if (comando_accion.separa()[0]=="03"):
+        logging.info("Habilitando socket TCP para recepcion y envio de archivo")                  
+        recibe = hiloTCP(topic)
+        recibe.hiloRecibidor.start()
+    elif (comando_accion.separa()[0]=="04"):
+        logging.debug("**************************************")            
+        logging.debug("Se recibio ALIVE de: "+str(topic))
+    else:
+        logging.debug("Comando no encontrado")
+    time.sleep(5)
+
 
 #Configuracion inicial de logging
 logging.basicConfig(
-    level = logging.DEBUG, 
+    level = logging.INFO, 
     format = '[%(levelname)s] (%(threadName)-10s) %(message)s'
     )
 
-#Callback que se ejecuta cuando nos conectamos al broker
-def on_connect(client, userdata, rc):
-    logging.info("Conectado al broker")	#No es necesario, pero pueees
-
-#Callback que se ejecuta cuando llega un mensaje al topic suscrito
-def on_message(client, userdata, msg):	#msg contiene el topic y la info que llego
-    #Se muestra en pantalla informacion que ha llegado
-    global dato 
-    dato = msg.payload
-    logging.info("Ha llegado el mensaje al topic: " + str(msg.topic)) #de donde vino el mss
-    logging.info("El contenido del mensaje es: " + str(dato.decode('utf-8')))#que vino en el mss
-    
-def on_publish(client, userdata, mid): 
-    publishText = "Publicacion satisfactoria"
-    logging.debug(publishText)
-
-
-client = mqtt.Client(clean_session=True) #Nueva instancia de cliente, iniciamos con una #sesion limpia
-client.on_connect = on_connect #Se configura la funcion "Handler" cuando suceda la conexion
-client.on_message = on_message #Se configura la funcion "Handler" que se activa al llegar un mensaje a un topic subscrito
-client.on_publish = on_publish
-client.username_pw_set(MQTT_USER, MQTT_PASS) #Credenciales requeridas por el broker, user y pass
-client.connect(host=MQTT_HOST, port = MQTT_PORT) #Conectar al servidor remoto
-#host es la ip, y el puerto el puerto xD si lo dejamos vacio lo conecta al 1883 (CREO)
+client = MQTTconfig(clean_session=True)
+rc = client.run()   #SALU Corre la congiduracion 
 first =b'\x01$201700722$4000'
 client.publish("comandos/08/201700722",first,2,False)
 
 #*********** Suscripciones del servidor ******************
 qos = 1
+'''
 user = configuracionesServidor(USER_FILENAME,qos)
 user.subUsuarios()
+'''
 salas = configuracionesServidor(SALAS_FILENAME,qos)
 salas.subSalas()
 comandos = configuracionesServidor(USER_FILENAME,qos)
@@ -202,18 +232,9 @@ client.loop_start()	#COn esto hacemos que las sub funcionen
 time.sleep(5)
 try:
     while True:
-        #logging.debug("El dato ingresado es: "+str(dato.decode('utf-8')))
-        #comandoIn=dato.decode('utf-8')
-        comando_accion = comandosServidor(str(dato))
-        
-        if (comando_accion.separa()[0]=="03"):
-            print("**************************************")
-            recibe = hiloTCP(IP_ADDR)
-            recibe.hiloRecibidor.start()
-
-        time.sleep(10)	
-
-
+        logging.debug("Hilo principal")
+        time.sleep(5)
+        	
 except KeyboardInterrupt:
     logging.warning("Desconectando del broker...")
 
